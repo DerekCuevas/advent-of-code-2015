24f7d500865dba4947213bbe864f8d2ca8a9d46a
Derek Cuevas <dccuevas2@gmail.com>
2016-07-13 16:25:20 -1000

    small refactor

--- not-quite-lisp/src/not_quite_lisp/core.clj
+++ not-quite-lisp/src/not_quite_lisp/core.clj
@@ -6,12 +6,12 @@
        (map #(case % \( 1 \) -1))
        (reduce +)))
 
-(defn travel-to-basement [directions]
+(defn floors-to-basement [directions]
   (reduce-kv
    (fn [floor position direction]
-     (let [next-floor (case direction \( (inc floor) \) (dec floor))]
+     (let [next-floor (+ floor direction)]
        (if (= next-floor -1)
          (reduced (inc position))
          next-floor)))
    0
-   (vec directions)))
+   (mapv #(case % \( 1 \) -1) directions)))
