7581f0597641c69b33a8d8cfe6b44f2ecfdbe142
Derek Cuevas <dccuevas2@gmail.com>
2016-07-14 09:36:05 -1000

    rough first solution for day two

---
+++ no-math/src/no_math/core.clj
@@ -0,0 +1,29 @@
+(ns no-math.core
+  (:require [clojure.string :as str])
+  (:gen-class))
+
+(def input (str/split (slurp "resources/input.txt") #"\n"))
+
+(defn str->dims [dim-str]
+  (->> (str/split dim-str #"x")
+       (map read-string)
+       (zipmap [:l :w :h])))
+
+(defn side-areas [{:keys [l w h]}]
+  (list (* l w) (* w h) (* h l)))
+
+(defn surface-area [dims]
+  (->> (side-areas dims)
+       (map #(* 2 %))
+       (reduce +)))
+
+(defn area-of-smallest-side [dims]
+  (apply min (side-areas dims)))
+
+(defn required-wrapping-paper [dims]
+  (+ (surface-area dims) (area-of-smallest-side dims)))
+
+(defn total-required-wrapping-paper [dims-list]
+  (->> dims-list
+       (map #(required-wrapping-paper (str->dims %)))
+       (reduce +)))
