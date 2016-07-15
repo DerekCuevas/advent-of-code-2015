1e5417dc10bcf34198747f8e6084cddf135845ee
Derek Cuevas <dccuevas2@gmail.com>
2016-07-14 19:41:42 -1000

    cleaning up

--- no-math/src/no_math/core.clj
+++ no-math/src/no_math/core.clj
@@ -4,23 +4,23 @@
 
 (def input (str/split (slurp "resources/input.txt") #"\n"))
 
-(defn str->dims [dim-str]
+(defn- str->dims [dim-str]
   (->> (str/split dim-str #"x")
        (map read-string)
        (zipmap [:l :w :h])))
 
-(defn areas [{:keys [l w h]}]
+(defn- areas [{:keys [l w h]}]
   (list (* l w) (* w h) (* h l)))
 
-(defn surface-area [dims]
+(defn- surface-area [dims]
   (->> (areas dims)
-       (map #(* 2 %))
+       (map (partial * 2))
        (reduce +)))
 
-(defn slack [dims]
+(defn- slack [dims]
   (apply min (areas dims)))
 
-(defn required-wrapping-paper [dims]
+(defn- required-wrapping-paper [dims]
   (+ (surface-area dims) (slack dims)))
 
 (defn total-required-wrapping-paper [dims-list]
@@ -28,17 +28,16 @@
        (map (comp required-wrapping-paper str->dims))
        (reduce +)))
 
-(defn smallest-perimeter [dims]
-  (->> (vals dims)
-       sort
+(defn- smallest-perimeter [dims]
+  (->> (sort (vals dims))
        (take 2)
-       (map #(+ % %))
+       (map (partial * 2))
        (apply +)))
 
-(defn ribbon-for-bow [dims]
-  (reduce * (vals dims)))
+(defn- ribbon-for-bow [{:keys [l w h]}]
+  (* l w h))
 
-(defn required-ribbon [dims]
+(defn- required-ribbon [dims]
   (+ (smallest-perimeter dims) (ribbon-for-bow dims)))
 
 (defn total-required-ribbon [dims-list]
