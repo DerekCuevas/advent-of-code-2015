236dd6681bca2f8f9876314c81e35ee7a4299e8f
Derek Cuevas <dccuevas2@gmail.com>
2016-07-16 09:20:32 -1000

    cleaning up

--- day-four/src/day_four/core.clj
+++ day-four/src/day_four/core.clj
@@ -1,16 +1,16 @@
 (ns day-four.core
-  (:require [digest :refer [md5]])
+  (:require [clojure.string :refer [starts-with?]]
+            [digest :refer [md5]])
   (:gen-class))
 
-(defn- starts-with-at-least-n-zeros? [n s]
-  (and (>= (count s) n)
-       (= (subs s 0 n) (apply str (repeat n "0")))))
+(defn- starts-with-n-zeros? [n s]
+  (starts-with? s (apply str (repeat n "0"))))
 
-(defn- find-smallest-md5-with-n-zeros [n key]
+(defn- find-smallest-md5 [n key]
   (loop [x 0]
-    (if (starts-with-at-least-n-zeros? n (md5 (str key x)))
+    (if (starts-with-n-zeros? n (md5 (str key x)))
       x
       (recur (inc x)))))
 
-(def find-smallest-md5-with-five-zeros (partial find-smallest-md5-with-n-zeros 5))
-(def find-smallest-md5-with-six-zeros (partial find-smallest-md5-with-n-zeros 6))
+(def find-smallest-md5-with-five-zeros (partial find-smallest-md5 5))
+(def find-smallest-md5-with-six-zeros (partial find-smallest-md5 6))
