c9d3ded5834776bd3da7c8796375ebdd1fd31fed
Derek Cuevas <dccuevas2@gmail.com>
2016-07-17 15:17:55 -1000

    added day five part two solution

--- day-five/src/day_five/core.clj
+++ day-five/src/day_five/core.clj
@@ -2,23 +2,26 @@
   (:require [clojure.string :refer [includes?]])
   (:gen-class))
 
+(def ^:private not-empty? (comp not empty?))
+
 (defn- contains-at-least-three-vowels? [s]
   (>= (count (filter #{\a \e \i \o \u} s)) 3))
 
 (defn- any-twice-in-a-row? [s]
-  (->> (partition-by identity s)
-       (map count)
-       (some (partial <= 2))))
+  (not= (count s) (count (dedupe s))))
 
 (defn- not-in-blacklist? [s]
   (->> ["ab" "cd" "pq" "xy"]
        (map (partial includes? s))
        (not-any? true?)))
 
-(def ^:private rules-a
-  (juxt contains-at-least-three-vowels?
-        any-twice-in-a-row?
-        not-in-blacklist?))
+(defn- pair-appears-twice? [s]
+  (some? (re-find #"([a-z][a-z]).*\1" s)))
+
+(defn- one-letter-repeats-with-one-between? [s]
+  (->> (partition 3 1 s)
+       (filter #(= (first %) (last %)))
+       not-empty?))
 
 (defn- nice? [rules s]
   (every? true? (rules s)))
@@ -26,5 +29,17 @@
 (defn nice-count [rules coll]
   (count (filter (partial nice? rules) coll)))
 
+(def ^:private rules-one
+  (juxt contains-at-least-three-vowels?
+        any-twice-in-a-row?
+        not-in-blacklist?))
+
 (defn nice-count-one [coll]
-  (nice-count rules-a coll))
+  (nice-count rules-one coll))
+
+(def ^:private rules-two
+  (juxt pair-appears-twice?
+        one-letter-repeats-with-one-between?))
+
+(defn nice-count-two [coll]
+  (nice-count rules-two coll))
--- day-five/test/day_five/core_test.clj
+++ day-five/test/day_five/core_test.clj
@@ -8,3 +8,8 @@
 (deftest nice-count-one-test
   (testing "returns number of nice strings in list"
     (is (= (nice-count-one input) 238))))
+
+(deftest nice-count-two-test
+  (testing "returns number of nice strings in list"
+    (is (= (nice-count-two input) 69))))
+
