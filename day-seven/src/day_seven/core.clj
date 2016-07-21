abae94f242d15c75b30b96876aae447ace3a7c9c
Derek Cuevas <dccuevas2@gmail.com>
2016-07-21 06:48:00 -1000

    cleaning up day seven parse functions

--- day-seven/src/day_seven/core.clj
+++ day-seven/src/day_seven/core.clj
@@ -4,7 +4,7 @@
 
 (def ^:private input (split (slurp "resources/input.txt") #"\n"))
 
-;; bit-not?
+;; double check bit-not
 (def ^:private gate->fn
   {"AND" bit-and
    "OR" bit-or
@@ -12,29 +12,18 @@
    "RSHIFT" bit-shift-right
    "NOT" bit-not})
 
-(defn- number-or-keyword [s]
-  (let [read (read-string s)]
-    (if (number? read) read (keyword read))))
+(defn- parse-input [s]
+  (let [input (read-string s)]
+    (if (number? input) input (keyword input))))
 
-;; maybe use multi-method here for dynamic dispatch
-;; on :type (no-op, unary, binary)?
-(defn- parse-no-op [[i]]
-  {:input (number-or-keyword i)})
+(defn- parse-op
+  ([i]
+    {:input (parse-input i)})
+  ([gate i]
+    {:input [(parse-input i)] :gate (gate->fn gate)})
+  ([ia gate ib]
+    {:input [(parse-input ia) (parse-input ib)] :gate (gate->fn gate)}))
 
-(defn- parse-unary-op [[gate i]]
-  {:input [(number-or-keyword i)] :gate (gate->fn gate)})
-
-(defn- parse-binary-op [[ia gate ib]]
-  {:input [(number-or-keyword ia) (number-or-keyword ib)]
-   :gate (gate->fn gate)})
-
-(def ^:private lhs-count->parse-fn
-  {1 parse-no-op
-   2 parse-unary-op
-   3 parse-binary-op})
-
-;; {:output _ :input [_ _] :gate}
 (defn- parse-instruction [s]
   (let [[lhs [rhs]] (map #(split % #" ") (split s #" -> "))]
-    (merge {:output (keyword rhs)}
-           ((lhs-count->parse-fn (count lhs)) lhs))))
+    (merge {:output (keyword rhs)} (apply parse-op lhs))))
