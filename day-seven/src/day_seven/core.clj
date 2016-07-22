43250078d1bfb9ee84607805c0f32475fefd8ebb
Derek Cuevas <dccuevas2@gmail.com>
2016-07-21 18:17:17 -1000

    added solutions and tests for day seven

--- day-seven/src/day_seven/core.clj
+++ day-seven/src/day_seven/core.clj
@@ -2,9 +2,6 @@
   (:require [clojure.string :refer [split]])
   (:gen-class))
 
-(def ^:private input (split (slurp "resources/input.txt") #"\n"))
-
-;; FIXME: double check bit-not !!!
 (def ^:private gate->fn
   {:AND bit-and
    :OR bit-or
@@ -27,12 +24,15 @@
 (defn- parse-instruction [s]
   (let [[lhs rhs] (split s #" -> ")]
     (merge {:output (keyword rhs)}
-           (->> (split lhs #" ") (map parse-input) (apply parse-op)))))
+           (->> (split lhs #" ")
+                (map parse-input)
+                (apply parse-op)))))
 
 (defn- add-connection [circuit {:keys [gate input output]}]
-  (assoc circuit output (if gate {:gate gate :input input} input)))
+  (->> (if gate {:gate gate :input input} input)
+       (assoc circuit output)))
 
-(defn- build-circuit [instructions]
+(defn build-circuit [instructions]
   (->> (map parse-instruction instructions)
        (reduce add-connection {})))
 
--- day-seven/test/day_seven/core_test.clj
+++ day-seven/test/day_seven/core_test.clj
@@ -1,7 +1,14 @@
 (ns day-seven.core-test
   (:require [clojure.test :refer :all]
+            [clojure.string :refer [split]]
             [day-seven.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def input (split (slurp "resources/input.txt") #"\n"))
+
+(def circuit-one (build-circuit input))
+(def circuit-two (assoc circuit-one :b 16076))
+
+(deftest probe-test
+  (testing "returns signal on wire in circuit"
+    (is (= (probe circuit-one :a) 16076))
+    (is (= (probe circuit-two :a) 2797))))
