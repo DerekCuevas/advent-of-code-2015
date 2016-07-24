a2d768ee65c2e496ef2bff6de85eceb088d7ef70
Derek Cuevas <dccuevas2@gmail.com>
2016-07-23 18:32:13 -1000

    added part two test

--- day-ten/test/day_ten/core_test.clj
+++ day-ten/test/day_ten/core_test.clj
@@ -4,4 +4,5 @@
 
 (deftest repeatedly-look-and-say-test
   (testing "returns the count of 40 repeated look-and-say's of an input string"
-    (is (= (repeatedly-look-and-say 40 "1113122113") 360154))))
+    (is (= (repeatedly-look-and-say 40 "1113122113") 360154))
+    (is (= (repeatedly-look-and-say 50 "1113122113") 5103798))))
