9231cfe3c1fbdaa5067d71d0d6009919c11947ca
Derek Cuevas <dccuevas2@gmail.com>
2016-09-10 13:28:18 -0700

    added files for day-twelve solutions

---
+++ day-twelve/src/day_twelve/core.clj
@@ -0,0 +1,7 @@
+(ns day-twelve.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ day-twelve/test/day_twelve/core_test.clj
@@ -0,0 +1,7 @@
+(ns day-twelve.core-test
+  (:require [clojure.test :refer :all]
+            [day-twelve.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
