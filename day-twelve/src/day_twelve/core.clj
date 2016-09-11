40bca0e196f13f839f25d627e95ba490b12dbbe3
Derek Cuevas <dccuevas2@gmail.com>
2016-09-11 12:24:08 -0700

    added solution for day-twelve part two

--- day-twelve/src/day_twelve/core.clj
+++ day-twelve/src/day_twelve/core.clj
@@ -14,3 +14,19 @@
       (+ running-sum json)
     :else
       running-sum)))
+
+(defn sum-without-red
+ ([json]
+  (sum-without-red 0 json))
+ ([running-sum json]
+  (cond
+    (vector? json)
+      (reduce sum-without-red running-sum json)
+    (map? json)
+      (if (not-any? (partial = "red") (vals json))
+        (reduce sum-without-red running-sum (vals json))
+        running-sum)
+    (number? json)
+      (+ running-sum json)
+    :else
+      running-sum)))
--- day-twelve/test/day_twelve/core_test.clj
+++ day-twelve/test/day_twelve/core_test.clj
@@ -8,3 +8,7 @@
 (deftest sum-test
   (testing "sums all numbers in json"
     (is (= (sum input) 191164))))
+
+(deftest sum-without-red-test
+  (testing "sums all numbers in json, except for objects and children with key 'red'"
+    (is (= (sum-without-red input) 87842))))
